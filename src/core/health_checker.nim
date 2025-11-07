3f607ba2032ff196050392dad8dd22dfca00e71b
xyz32 <32@jcubed.dev>
2025-11-06 20:13:09 -0500

    renaming, solving null pointer dereference

--- src/core/health_checker.nim
+++ src/core/health_checker.nim
@@ -7,7 +7,7 @@ const
   HEALTH_CHECK_TIMEOUT = 2000     # 2 second timeout per check
 
 type
-  HealthChecker* = object
+  HealthCheckerContext = object
     pool: ptr BackendPool           # backend pool to monitor
     running: ptr bool               # checker running flag
     check_interval: int             # milliseconds between checks
@@ -34,40 +34,40 @@ proc probe_backend(host: string, port: int): bool =
 
 proc health_check_thread(arg: pointer) {.thread, gcsafe.} =
   ## check: periodic health check thread
-  let checker = cast[ptr HealthChecker](arg)
+  let ctx = cast[ptr HealthCheckerContext](arg)
   log_info("health", "health checker started")
   
-  while checker.running[]:
-    acquire(checker.pool.lock)
-    let backend_count = checker.pool.backends.len
-    release(checker.pool.lock)
+  while ctx.running[]:
+    acquire(ctx.pool.lock)
+    let backend_count = ctx.pool.backends.len
+    release(ctx.pool.lock)
     
     for backend_index in 0..<backend_count:
-      acquire(checker.pool.lock)
-      let backend = checker.pool.backends[backend_index]
-      release(checker.pool.lock)
+      acquire(ctx.pool.lock)
+      let backend = ctx.pool.backends[backend_index]
+      release(ctx.pool.lock)
       
       let is_alive = probe_backend(backend.host, backend.port)
       
       if is_alive != backend.alive:
-        acquire(checker.pool.lock)
-        checker.pool.backends[backend_index].alive = is_alive
-        release(checker.pool.lock)
+        acquire(ctx.pool.lock)
+        ctx.pool.backends[backend_index].alive = is_alive
+        release(ctx.pool.lock)
         
         if is_alive:
           log_info("health", backend.host & ":" & $backend.port & " is now alive")
         else:
           log_warn("health", backend.host & ":" & $backend.port & " is now dead")
     
-    sleep(checker.check_interval)
+    sleep(ctx.check_interval)
 
 proc start_health_checker*(pool: ptr BackendPool, running: ptr bool): Thread[pointer] =
   ## start: launch health checker thread
-  var checker = cast[ptr HealthChecker](alloc0(sizeof(HealthChecker)))
-  checker.pool = pool
-  checker.running = running
-  checker.check_interval = HEALTH_CHECK_INTERVAL
+  var ctx = cast[ptr HealthCheckerContext](alloc0(sizeof(HealthCheckerContext)))
+  ctx.pool = pool
+  ctx.running = running
+  ctx.check_interval = HEALTH_CHECK_INTERVAL
   
   var thread: Thread[pointer]
-  createThread(thread, health_check_thread, checker)
+  createThread(thread, health_check_thread, ctx)
   return thread
